c5aba0a55d08d595f4ae367016515ede3227ea02
Bubumariia Kudaiberdieva <mariiakudaiberdieva@gmail.com>
2022-10-09 21:29:17 +0300

    first commit, fetch API

---
+++ src/components/Tour.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export default class Tour extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container">
+          <img src="" alt="" />
+          <div>
+            <h2></h2>
+            <h2></h2>
+          </div>
+          <p></p>
+          <button>read more</button>
+          <button>not interested</button>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
---
+++ src/components/Tours.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Tours extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div></div>;
+  }
+}
